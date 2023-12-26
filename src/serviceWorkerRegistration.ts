bf29a48472951167ee75ea7af30db6be1b000b69
Pankaj <pankaj1@tothenew.com>
2023-12-26 10:40:33 +0530

    update

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -115,6 +115,12 @@ function registerValidSW(swUrl: string, config?: Config) {
 }
 
 // Listen for messages from the service worker
+navigator.serviceWorker.addEventListener('message', (event) => {
+  if (event.data.type === 'SKIP_WAITING') {
+    // Reload the page to activate the new service worker
+    window.location.reload();
+  }
+});
 
 function checkValidServiceWorker(swUrl: string, config?: Config) {
   // Check if the service worker can be found. If it can't reload the page.
