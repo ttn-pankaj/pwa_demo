2523b5ea1bacaac7e6b25355bc9fd10fc1e12725
Pankaj <pankaj1@tothenew.com>
2023-12-26 10:14:09 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>Hello Test 5</h1>
+       <h1>Hello Test 6</h1>
       </header>
     </div>
   );
